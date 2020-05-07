62690878276bd5911299be07d534cd9b3186b3c2
Hamza <hm2974022@gmail.com>
2020-05-07 17:41:03 +0500

    Added files

---
+++ android/app/src/main/kotlin/com/travelfix/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.travelfix
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,6 +1,5 @@
 import UIKit
 import Flutter
-import GoogleMaps
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -8,7 +7,6 @@ import GoogleMaps
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GMSServices.provideAPIKey("AIzaSyDa9ISNTUGgDP08FKdGL9a5MQ5o01jgPbQ")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
